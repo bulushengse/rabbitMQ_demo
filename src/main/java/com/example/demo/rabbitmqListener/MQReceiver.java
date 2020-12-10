a3df395f2aad75a527d6f9c1a975d6d5237ec8f1
bulushengse <314108271@qq.com>
2020-12-10 16:44:13 +0800

    3

--- src/main/java/com/example/demo/rabbitmqListener/MQReceiver.java
+++ src/main/java/com/example/demo/rabbitmqListener/MQReceiver.java
@@ -11,7 +11,7 @@ import com.rabbitmq.client.Channel;
 @Component
 public class MQReceiver {
 	
-	@RabbitListener(queues = "direct_queue1")
+	@RabbitListener(queues = "direct_queue")
 	public void directReceive(Message message, Channel channel) throws IOException {
 		// 手动应答
 		// 第一个参数代表消费者拒绝一条或者多条消息，第二个参数表示一次是否拒绝多条消息，第三个参数表示是否把当前消息重新入队
